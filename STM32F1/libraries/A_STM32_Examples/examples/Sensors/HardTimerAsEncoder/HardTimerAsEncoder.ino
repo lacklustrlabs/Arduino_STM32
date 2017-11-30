7b4118233f264f359cd9f4a619f0686658147dbf
lacklustrlabs <lacklustrlabs@gmail.com>
2017-11-30 19:56:11 +0100

    Using PA0 and PA1 as encoder input. Major cleanup

--- STM32F1/libraries/A_STM32_Examples/examples/Sensors/HardTimerAsEncoder/HardTimerAsEncoder.ino
+++ STM32F1/libraries/A_STM32_Examples/examples/Sensors/HardTimerAsEncoder/HardTimerAsEncoder.ino
@@ -6,16 +6,16 @@
  * The default behavior is for quadrature encoder. 
  * 
  * To avoid overflowing the encoder (which may or may not happen (although with 16 bits, it's likely), the following code 
- * will interrupt every time the number of pulses that each revolution gives to increment/decrement a variable (ints). 
+ * will interrupt every time the number of pulses that each revolution gives to increment/decrement a variable (revolutions). 
  * 
- * This means that the total number of pulses given by the encoder will be (ints * PPR) + timer.getCount()
+ * This means that the total number of pulses given by the encoder will be (revolutions * PPR) + timer.getCount()
  * 
  * Attached is also a bit of code to simulate a quadrature encoder. 
  * To test this library, make the connections as below: 
  * 
- * TIMER2 inputs -> Digital Pins used to simulate.
- * D2 -> 1K Ohm -> D4
- * D3 -> 1K Ohm -> D5
+ * TIMER2 inputs -> current limiting resistor -> Digital Pins used to simulate.
+ * PA0 -> 1K Ohm -> D4
+ * PA1 -> 1K Ohm -> D5
  * 
  * COUNTING DIRECTION: 
  * 0 means that it is upcounting, meaning that Channel A is leading Channel B
@@ -26,94 +26,52 @@
  * mode 2 - only counts pulses on Channel A
  * mode 3 - counts on both channels. 
  * 
-*/
-
-
+ */
 
 #include "HardwareTimer.h"
 
 //Encoder simulation stuff
 //NOT NEEDED WHEN CONNECTING A REAL ENCODER
-unsigned char mode = 0;  //to issue steps...
-unsigned char dir = 'F'; // direction of movement of the encoder.
 
-unsigned int freq = 100; //update frequency.
-unsigned long time = 0;        //time variable for millis()
-unsigned char states[4];       //because I'm lazy...
-
+const uint8_t output_a = D4;
+const uint8_t output_b = D5;
 
 //Encoder stuff
+const uint8_t input_a = PA0; // D11 this should be the pin of timer 2 channel 1
+const uint8_t input_b = PA1; // D10 this should be the pin of timer 2 channel 2
 
 //Pulses per revolution
 #define PPR   1024
 
 HardwareTimer timer(2);
 
-volatile unsigned long ints = 0;
+volatile unsigned long revolutions = 0;
 
-void func(){
-if (timer.getDirection()){
-  ints--;
-} else{
-  ints++;
-}
-}
-
-
-void setup() {
-  //define the Timer channels as inputs. 
-  pinMode(D2, INPUT_PULLUP);  //channel A
-  pinMode(D3, INPUT_PULLUP);  //channel B
-
-//configure timer as encoder
-  //set mode, the channel is not used when in this mode.
-  // You must, however, use a valid channel number.
-  timer.setMode(TIMER_CH1, TIMER_ENCODER); 
-  timer.pause(); //stop... 
-  timer.setPrescaleFactor(1); //normal for encoder to have the lowest or no prescaler. 
-  timer.setOverflow(PPR);    //use this to match the number of pulse per revolution of the encoder. Most industrial use 1024 single channel steps. 
-  timer.setCount(0);          //reset the counter. 
-  timer.setEdgeCounting(TIMER_SMCR_SMS_ENCODER3); //or TIMER_SMCR_SMS_ENCODER1 or TIMER_SMCR_SMS_ENCODER2. This uses both channels to count and ascertain direction. 
-  timer.attachInterrupt(0, func); //channel doesn't mean much here either.  
-  timer.resume();                 //start the encoder... 
-  
-//Setup encoder simulator  
-  pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
-
-  digitalWrite(5, HIGH); 
-  digitalWrite(4, LOW);
-  states[0] = 0; //00
-  states[1] = 1; //01
-  states[2] = 3; //11
-  states[3] = 2; //10
-
-
-}
-
-//Support variables.
-unsigned long interval=0; //variable for status updates... 
-char received = 0;
-
-void loop() {
-  //encoder code
-  if (millis() - interval >= 1000) { 
-     Serial.print(timer.getCount()); 
-     Serial.println(" counts");
-     Serial.print("direction ");
-     Serial.println(timer.getDirection());
-     Serial.print("Full Revs: ");
-     Serial.println(ints);
-     interval = millis(); //update interval for user. 
+void overflowInterrupt(){
+  if (timer.getDirection()){
+    revolutions--;
+  } else{
+    revolutions++;
   }
-  
-  
-  
+}
+
+void simulateStep(uint8_t step){
+  static const unsigned char states[] = {0,1,3,2};//{0b00,0b01,0b11,0b10};
+  digitalWrite(output_a, (states[step%4] & 0x01));
+  digitalWrite(output_b, (states[step%4] >> 1));
+}
+
 /*
-EENCODER SIMULATION PART. 
+ENCODER SIMULATION PART. 
 
 
 */  
+void simulate() {
+  static unsigned long updatePeriod = 100; //update period.
+  static unsigned long lastSimulationAt = 0;  //time variable for millis()
+  static unsigned char mode = 0;  //to issue steps...
+  static unsigned char dir = 'F'; // direction of movement of the encoder.
+
 /*
  * 
  * Protocol... 
@@ -131,29 +89,73 @@ EENCODER SIMULATION PART.
 
 //take care of comms...
   if (Serial.available() > 0) {
-    received = Serial.read();
+    char received = Serial.read();
     if (received == 'F' || received == 'R') dir = received; //direction. Forward or Reverse.
     if (received == '1') timer.setEdgeCounting(TIMER_SMCR_SMS_ENCODER1); //count only the pulses from input 1
     if (received == '2') timer.setEdgeCounting(TIMER_SMCR_SMS_ENCODER2); //count only the pulses from input 2
     if (received == '3') timer.setEdgeCounting(TIMER_SMCR_SMS_ENCODER3); //count on both channels (default of the lib).
     if (received == '4') timer.setPrescaleFactor(4); //only updates on overflow, so you need to wait for an overflow. Not really used...  
     if (received == '0') timer.setPrescaleFactor(1); //only updates on overflow, so you need to wait for an overflow. 
-    if (received == '-') freq+=50; //increase speed.
+    if (received == '-') updatePeriod+=50; //decrease speed.
     if (received == '+') {
-      freq-=50;
-      if (freq <= 0) freq = 100; //smallest is 10 ms. 
+      updatePeriod-=50;
+      if (updatePeriod <= 0) updatePeriod = 50; //smallest is 50 ms. 
     }
   }
   
 //simulate encoder pulses.   
-  if ( millis() - time >= freq) { 
-    time = millis(); //prepare next
-    
+  
+  if ( millis() - lastSimulationAt >= updatePeriod) { 
+    lastSimulationAt = millis(); //prepare next
+		
     if (dir == 'F')  mode++;
-    if (dir == 'R')  mode --;
-    
-    digitalWrite(4, (states[mode%4] & 0x01));
-    digitalWrite(5, (states[mode%4] >> 1));
-  }
+    if (dir == 'R')  mode--;
 
+    simulateStep(mode);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial)
+    ;
+  //define the Timer channels as inputs. 
+  pinMode(input_a, INPUT_PULLUP);  //channel A
+  pinMode(input_b, INPUT_PULLUP);  //channel B
+
+//configure timer as encoder
+  //set mode, the channel is not used when in this mode.
+  // You must, however, use a valid channel number.
+  timer.setMode(TIMER_CH1, TIMER_ENCODER); 
+  timer.pause(); //stop... 
+  timer.setPrescaleFactor(1); //normal for encoder to have the lowest or no prescaler. 
+  timer.setOverflow(PPR);    //use this to match the number of pulse per revolution of the encoder. Most industrial use 1024 single channel steps. 
+  timer.setCount(0);          //reset the counter. 
+  timer.setEdgeCounting(TIMER_SMCR_SMS_ENCODER3); //or TIMER_SMCR_SMS_ENCODER1 or TIMER_SMCR_SMS_ENCODER2. This uses both channels to count and ascertain direction. 
+  timer.attachInterrupt(0, overflowInterrupt); //channel must be 0 here.  
+  timer.resume();                 //start the encoder... 
+  
+//Setup encoder simulator  
+  pinMode(output_a, OUTPUT);
+  pinMode(output_b, OUTPUT);
+  simulateStep(0);
+}
+
+void loop() {
+  //encoder code
+  static uint32_t lastMessageAt=0; //variable for status updates... 
+  if (millis() - lastMessageAt >= 1000) { 
+    Serial.print(timer.getCount()); 
+    Serial.println(" counts");
+    Serial.print("direction ");
+    Serial.println(timer.getDirection());
+    //Serial.print(" should be ");
+    //Serial.println(dir=='F'?1:0);
+    Serial.print("Full Revs: ");
+    Serial.println(revolutions);
+    lastMessageAt = millis();
+  }
+  
+  // simulator code 
+  simulate();
 }
